35642d191c34c5004293b671b7142d731fdc9867
tosha504 <tosha.rovniy@gmail.com>
2021-05-22 22:05:22 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function($) {
+  const $burger = $(".header__burger");
+  const $headerNav = $(".header__nav");
+  const $main = $(".main-screen");
+  const $headerBtn = $(".header__btn");
+  const $header = $(".header");
+
+  $(window).on(
+    "scroll",
+    $.debounce(0, function() {
+      if ($(this).scrollTop() > 20) {
+        $header.addClass("sticky");
+      } else {
+        $header.removeClass("sticky");
+      }
+    })
+  );
+
+  $headerBtn.on("click", function() {
+    $(".dialog").addClass("active");
+  });
+
+  $(".dialog__close").on("click", function() {
+    $(".dialog").removeClass("active");
+  });
+
+  // $main.fadeOut().fadeIn(3000);
+
+  $burger.on("click", function() {
+    $headerNav.toggleClass("active");
+    $burger.toggleClass("active");
+    $("html").toggleClass("fixed-page");
+  });
+
+  $(".services__item-subtitle").on("click", function() {
+    $(this)
+      .toggleClass("active")
+      .next()
+      .slideToggle(300);
+  });
+
+  let $Seconds = $(".time").text(),
+    int;
+  int = setInterval(function() {
+    if ($Seconds > 0) {
+      $Seconds--;
+      $(".time").text($Seconds);
+      блок;
+    } else {
+      clearInterval(int);
+      alert("Time is out!");
+    }
+  }, 1000);
+
+  $(".header__link").on("click", function(e) {
+    e.preventDefault();
+    const target = $(this).attr("href");
+    $("html, body").animate({ scrollTop: $(target).offset().top }, 800);
+  });
+});

0ecce85e15235853eaf81b47b13afd49c179a8c0
tosha504 <tosha.rovniy@gmail.com>
2021-09-06 22:40:13 +0200

    clear JS and translate

--- src/index.js
+++ src/index.js
@@ -1,60 +1,89 @@
-jQuery(document).ready(function($) {
-  const $burger = $(".header__burger");
-  const $headerNav = $(".header__nav");
-  const $main = $(".main-screen");
-  const $headerBtn = $(".header__btn");
-  const $header = $(".header");
+window.addEventListener("DOMContentLoaded", () => {
+  const burger = document.querySelector(".header__burger"),
+    headerNav = document.querySelector(".header__nav"),
+    main = document.querySelector(".main-screen"),
+    headerBtn = document.querySelector(".header__btn"),
+    header = document.querySelector(".header"),
+    dialog = document.querySelector(".dialog"),
+    html = document.querySelector("html"),
+    tabs = document.querySelectorAll(".services__column-items");
 
-  $(window).on(
-    "scroll",
-    $.debounce(0, function() {
-      if ($(this).scrollTop() > 20) {
-        $header.addClass("sticky");
-      } else {
-        $header.removeClass("sticky");
-      }
-    })
-  );
-
-  $headerBtn.on("click", function() {
-    $(".dialog").addClass("active");
-  });
-
-  $(".dialog__close").on("click", function() {
-    $(".dialog").removeClass("active");
-  });
-  $header.fadeOut().fadeIn(2500);
-  $main.fadeOut().fadeIn(2500);
-
-  $burger.on("click", function() {
-    $headerNav.toggleClass("active");
-    $burger.toggleClass("active");
-    $("html").toggleClass("fixed-page");
-  });
-
-  $(".services__item-subtitle").on("click", function() {
-    $(this)
-      .toggleClass("active")
-      .next()
-      .slideToggle(300);
-  });
-
-  let $Seconds = $(".time").text(),
-    int;
-  int = setInterval(function() {
-    if ($Seconds > 0) {
-      $Seconds--;
-      $(".time").text($Seconds);
-      блок;
+  window.addEventListener("scroll", function() {
+    if (this.scrollY > 20) {
+      header.classList.add("sticky");
     } else {
-      clearInterval(int);
-      alert("Time is out!");
+      header.classList.remove("sticky");
     }
-  }, 1000);
-
-  $(".header__link").on("click", function(e) {
-    e.preventDefault();
-    const target = $(this).attr("href");
-    $("html, body").animate({ scrollTop: $(target).offset().top }, 800);
   });
+
+  headerBtn.addEventListener("click", function() {
+    dialog.classList.add("active");
+  });
+
+  document
+    .querySelector(".dialog__close")
+    .addEventListener("click", function() {
+      dialog.classList.remove("active");
+    });
+
+  function opacity() {
+    header.classList.add("opacity");
+    main.classList.add("opacity");
+  }
+  opacity();
+
+  burger.addEventListener("click", function() {
+    headerNav.classList.toggle("active");
+    burger.classList.toggle("active");
+    html.classList.toggle("fixed-page");
+  });
+
+  function log() {
+    tabs.forEach((tab) => {
+      tab.addEventListener("click", function(e, i) {
+        console.log(tabs);
+        tab.classList.toggle("active");
+        tab.classList.toggle("active");
+      });
+    });
+  }
+  log();
+
+  //   drop.addEventListener("click", function(e) {
+
+  //     // 	  .classList.toggle("active");
+  //     // document.querySelector(".services__item-text").classList.toggle("active");
+  //   });
+
+  //   function hideTabContent() {
+  //     tabs.forEach((item) => {
+  //       console.log(item.target);
+  //       item.style.display = "none";
+  //     });
+
+  //     //     document
+  //     //       .querySelector(".services__column-items")
+  //     //       .addEventListener("click", function() {
+  //     //         this.classList.toggle("active");
+  //     //         document
+  //     //           .querySelector(".services__item-text")
+  //     //           .classList.toggle("active");
+  //     //         //     // .slideToggle(300);
+  //     //       });
+  //   }
+  //   hideTabContent();
+  const anchors = document.querySelectorAll('a[href*="#"]');
+
+  for (let anchor of anchors) {
+    anchor.addEventListener("click", function(e) {
+      e.preventDefault();
+
+      const blockID = anchor.getAttribute("href").substr(1);
+
+      document.getElementById(blockID).scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    });
+  }
 });
